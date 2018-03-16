d4f4411223fe7ae161b0d4500cae83afb59add54
Martin Severin Steffensen <maritim@gmail.com>
2018-03-16 11:57:06 +0100

    Initial commit.

---
+++ FluentValidationDeserialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentValidationDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validationFailure = new ValidationFailure("", "this is an error");
+            var serialized = JsonConvert.SerializeObject(validationFailure);
+            var deserialized = JsonConvert.DeserializeObject<ValidationFailure>(serialized);
+        }
+    }
+}
